332306883c98bcb383be4e6dcacc193141d02ccb
sdc9787 <sfc9787@gmail.com>
2025-08-29 11:55:34 +0900

    chore(test): MSW 서버 경로 alias(@)로 수정 및 tsconfig.jest baseUrl 추가

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,25 +1,28 @@
-import '@testing-library/jest-dom';
-import 'whatwg-fetch';
+import "@testing-library/jest-dom";
+import "whatwg-fetch";
 
 // MSW setup (tests only). The server is started in global setup style here.
 // If you later need a custom globalSetup file, move this logic there.
-import { server } from './src/mocks/server';
+// Use absolute path alias for clarity in tests.
+import { server } from "@/mocks/server";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 // Polyfill IntersectionObserver for framer-motion in JSDOM
-if (typeof window !== 'undefined' && !('IntersectionObserver' in window)) {
-	// @ts-ignore
-	window.IntersectionObserver = class IntersectionObserver {
-		constructor() {}
-		observe() {}
-		unobserve() {}
-		disconnect() {}
-		takeRecords() { return []; }
-		root = null;
-		rootMargin = '';
-		thresholds = [];
-	};
+if (typeof window !== "undefined" && !("IntersectionObserver" in window)) {
+  // @ts-ignore
+  window.IntersectionObserver = class IntersectionObserver {
+    constructor() {}
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+    takeRecords() {
+      return [];
+    }
+    root = null;
+    rootMargin = "";
+    thresholds = [];
+  };
 }
