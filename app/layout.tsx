a9ca5ff4c076b1e179d5f957fcf8e6e6a3fb6138
sdc9787 <sfc9787@gmail.com>
2025-09-01 10:59:18 +0900

    feat/index.ts 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,25 @@
-import type { Metadata } from "next";
-import { Noto_Sans_KR, Roboto } from "next/font/google";
-import "./globals.css";
-import { MswProvider } from "@/app/msw-provider";
-import { QueryProvider } from "@/app/query-provider";
+import type { Metadata } from 'next';
+import { Noto_Sans_KR, Roboto } from 'next/font/google';
+import './globals.css';
+import { MswProvider, QueryProvider } from '@/app';
 
 const notoSansKR = Noto_Sans_KR({
-  variable: "--font-noto-sans-kr",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], // 실제 사용하는 weight만 로드
-  display: "swap", // 폰트 로딩 최적화
+  variable: '--font-noto-sans-kr',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'], // 실제 사용하는 weight만 로드
+  display: 'swap', // 폰트 로딩 최적화
 });
 
 const roboto = Roboto({
-  variable: "--font-roboto",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], // 실제 사용하는 weight만 로드
-  display: "swap", // 폰트 로딩 최적화
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'], // 실제 사용하는 weight만 로드
+  display: 'swap', // 폰트 로딩 최적화
 });
 
 export const metadata: Metadata = {
-  title: "CV Mento",
-  description: "AIBE2_FinalProject_CodeHansabari_FE",
+  title: 'CV Mento',
+  description: 'AIBE2_FinalProject_CodeHansabari_FE',
 };
 
 export default function RootLayout({
