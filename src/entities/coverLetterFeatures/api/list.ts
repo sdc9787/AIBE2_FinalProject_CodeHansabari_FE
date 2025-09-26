1b9111b2401e73c5a9eb0680cb3517bd8c4c5b36
sdc9787 <sfc9787@gmail.com>
2025-09-26 19:34:18 +0900

    fix : 중복제거 조회 api / 추가

--- src/entities/coverLetterFeatures/api/list.ts
+++ src/entities/coverLetterFeatures/api/list.ts
@@ -2,6 +2,6 @@ import { clientFetch } from '@/shared';
 import type { ApiFeaturePageResponse } from '../model/type';
 
 export const fetchCoverLetterFeatures = (page = 0, size = 20, sort?: string) =>
-  clientFetch.get<ApiFeaturePageResponse>('/api/cover-letter-features', {
+  clientFetch.get<ApiFeaturePageResponse>('/api/cover-letter-features/', {
     params: { page, size, sort },
   });
