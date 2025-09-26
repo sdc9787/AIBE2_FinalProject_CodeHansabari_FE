1374ff04fd263200f64ac744a9960dfda87f128a
sdc9787 <sfc9787@gmail.com>
2025-09-26 19:39:49 +0900

    fix : 크롤링 / 추가

--- src/features/delete-crawl/api/deleteCrawl.ts
+++ src/features/delete-crawl/api/deleteCrawl.ts
@@ -2,7 +2,7 @@ import { clientFetch } from '@/shared';
 
 export const deleteCrawl = (crawlId?: number) => {
   if (!crawlId) {
-    return clientFetch.delete(`/api/crawled-cover-letters`);
+    return clientFetch.delete(`/api/crawled-cover-letters/`);
   }
 
   return clientFetch.delete(`/api/crawled-cover-letters/${crawlId}`);
